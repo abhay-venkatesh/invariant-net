e4c76796d10830a24b62d6270c21bdfc15ea1d3a
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-02-15 14:07:19 -0600

    Prep Sandman 2 Phase 4

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-# from lib.ThetaDFSegNet import ThetaDFSegNet
-from lib.UnfixedThetaSegNet import UnfixedThetaSegNet
+from lib.UnfixedMultiSegNet import UnfixedMultiSegNet 
 
 def main():
-    # dataset_directory = './datasets/Unreal-20View-11class/homoview1/'
-    dataset_directory = './datasets/Unreal-20View-11class/view0prime/'
-    test_directory = './datasets/Unreal-20View-11class/view0/'
-    net = UnfixedThetaSegNet(dataset_directory)
+    dataset_directory = './datasets/UnrealFlows/'
+    net = UnfixedMultiSegNet(dataset_directory)
     net.train(num_iterations=100000, theta=0, is_trainable=True, 
               dataset_directory=dataset_directory, learning_rate=1e-2, batch_size=5)
-
-    # net.test(theta=0, is_trainable=True, dataset_directory=test_directory, learning_rate=1e-2)
 
 if __name__ == "__main__":
     main()
