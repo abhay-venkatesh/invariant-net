28eeb01e80b0f38f01eb4f7493ba6a11d4418bea
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-03-03 14:10:20 -0600

    Update datasets gitignore

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+# Only use a single GPU when not testing
+if os.name != 'nt': 
+    os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+
 from lib.SegNetLogger import SegNetLogger 
 
 def main():
-    dataset_directory = './datasets/UnrealFlows/'
-    net = UnfixedMultiSegNet(dataset_directory)
-    net.train(num_iterations=100000, theta=0, is_trainable=True, 
-              dataset_directory=dataset_directory, learning_rate=1e-2, batch_size=5)
+    dataset_directory = './datasets/Unreal-20View-11class/'
+    net = SegNetLogger()
+    net.train(num_iterations=500, dataset_directory=dataset_directory, learning_rate=1e-2, batch_size=5)
+    # net.build_contextual_feedback_log(dataset_directory)
 
 if __name__ == "__main__":
     main()
