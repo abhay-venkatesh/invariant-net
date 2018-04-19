f22049c941a1ca6ee400fb059b08f72176449223
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-04-18 19:51:12 -0500

    More cleanup

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from lib.SegNetLogger import SegNetLogger
+from lib.InvariantNet import InvariantNet
 
 def main():
     dataset_directory = './datasets/Unreal-20View-11class/view0/'
 
-    net = SegNetLogger()
-    # net.train(dataset_directory, num_iterations=500, 
-    #         learning_rate=1e-2, batch_size=5)
-    net.build_contextual_feedback_log(dataset_directory)
+    net = InvariantNet()
+    net.train(dataset_directory, num_iterations=500, 
+              learning_rate=1e-2, batch_size=5)
 
 if __name__ == "__main__":
     main()
--- scripts/kill_processes.sh
+++
@@ -1,1 +0,0 @@
-kill $(nvidia-smi | awk '$2=="Processes:" {p=1} p && $2 == 2 && $3 > 0 {print $3}')
